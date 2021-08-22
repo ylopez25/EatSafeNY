d2dfd45cfb0f2d3f195a69bae4cdd3d3ed937a5e
Teyanna Earle <teyonna.earle92@gmail.com>
2021-08-22 17:48:18 -0400

    update

--- eatsafeny/src/pages/Results.js
+++ eatsafeny/src/pages/Results.js
@@ -6,7 +6,7 @@ function Results(props) {
     return (
         <div>
             <TopSearchBar {...props}/>
-            <h2>Search Results</h2>
+            <h2 id="search-h2">Search Results</h2>
             {props.error ? <p>Sorry, no search results found </p> : null}
             <ResultsList {...props} />
         </div>
