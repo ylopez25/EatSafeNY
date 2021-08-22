748b7a39d7a66e0c69f02e77a06a6f53a94421f2
Teyanna Earle <teyonna.earle92@gmail.com>
2021-08-21 22:28:50 -0400

    Add uuid

--- eatsafeny/src/components/ResultsList.js
+++ eatsafeny/src/components/ResultsList.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { v4 as uuidv4 } from 'uuid';
 import Result from "../components/Result.js";
+
 
 function ResultsList(props) {
   return (
     <div>
       {props.restaurants.map((restaurant) => {
-        return <Result restaurant={restaurant}/>;
+        return <Result key={uuidv4()} restaurant={restaurant}/>;
       })}
     </div>
   );
